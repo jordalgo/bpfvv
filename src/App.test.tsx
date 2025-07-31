78fe15cd2d2665268a4acda371f5b6b194919a62
Jordan Rome <jordalgo@meta.com>
2025-07-30 21:28:36 -0400

    Add test for new up/down keyboard behavior

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -137,7 +137,6 @@ describe("App", () => {
     const logContainerEl = document.getElementById("log-container");
     expect(logContainerEl).toBeTruthy();
     expect(logContainerEl).toBeVisible();
-    expect(document.getElementById("state-panel-shown")).toBeTruthy();
 
     const line5 = document.getElementById("line-5");
     const line6 = document.getElementById("line-6");
@@ -165,14 +164,14 @@ describe("App", () => {
 
     // Keyboard Down Arrow
     fireEvent.keyDown(logContainerEl, { key: "ArrowDown", code: "ArrowDown" });
+    expect(line5.classList.contains("selected-line")).toBeFalsy();
     expect(line6.classList.contains("selected-line")).toBeFalsy();
     expect(line7.classList.contains("selected-line")).toBeTruthy();
-    expect(line5.classList.contains("selected-line")).toBeFalsy();
 
     // Keyboard Up Arrow
     fireEvent.keyDown(logContainerEl, { key: "ArrowUp", code: "ArrowUp" });
+    expect(line5.classList.contains("selected-line")).toBeTruthy();
     expect(line6.classList.contains("selected-line")).toBeFalsy();
     expect(line7.classList.contains("selected-line")).toBeFalsy();
-    expect(line5.classList.contains("selected-line")).toBeTruthy();
   });
 });
